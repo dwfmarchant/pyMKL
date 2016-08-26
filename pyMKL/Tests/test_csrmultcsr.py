d65afd2c0055a0d227cdf4cde629494ed7bb2f1d
Dave Marchant <dwfmarchant@gmail.com>
2016-08-25 23:12:24 -0700

    Initial implementation of double precision sparse matrix-matrix multiplication (mkl_dcsrmultcsr).

---
+++ pyMKL/Tests/test_csrmultcsr.py
@@ -0,0 +1,46 @@
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+
+import unittest
+import pyMKL
+import scipy.sparse as sp
+
+class TestCsrmulcsr(unittest.TestCase):
+
+    nSize = 500
+    A = sp.rand(nSize, nSize, 0.05, format='csr', random_state=1)
+    B = sp.rand(nSize, nSize, 0.05, format='csr', random_state=2)
+
+    a = A.data
+    ja = A.indices+1
+    ia = A.indptr+1
+    b = B.data
+    jb = B.indices+1
+    ib = B.indptr+1
+    m,n,k = 3*[nSize]
+
+    def test_AmulB(self):
+        C_sp = self.A*self.B
+        c, jc, ic = pyMKL.mkl_dcsrmultcsr(self.m, self.n, self.k, 
+                                          self.a, self.ja, self.ia, 
+                                          self.b, self.jb, self.ib)
+        C_mkl = sp.csr_matrix((c, jc-1,ic-1),(self.m,self.k))
+        diff = C_sp - C_mkl
+        self.assertEquals(diff.nnz,0)
+
+    def test_ATmulB(self):
+        C_sp = self.A.T*self.B
+        c, jc, ic = pyMKL.mkl_dcsrmultcsr(self.m, self.n, self.k, 
+                                          self.a, self.ja, self.ia, 
+                                          self.b, self.jb, self.ib,
+                                          trans='T')
+        C_mkl = sp.csr_matrix((c, jc-1,ic-1),(self.m,self.k))
+        diff = C_sp - C_mkl
+        self.assertEquals(diff.nnz,0)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ pyMKL/mkl_csrmultcsr.py
@@ -0,0 +1,95 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+
+from . import MKLlib
+from ctypes import POINTER, byref, c_int, c_char, c_double
+import numpy as np
+
+_mkl_dcsrmultcsr = MKLlib.mkl_dcsrmultcsr
+_mkl_dcsrmultcsr.argtypes = [POINTER(c_char),   #trans
+                             POINTER(c_int),     #request
+                             POINTER(c_int),     #sort
+                             POINTER(c_int),     #m
+                             POINTER(c_int),     #n
+                             POINTER(c_int),     #k
+                             POINTER(c_double),  #a
+                             POINTER(c_int),     #ja
+                             POINTER(c_int),     #ia
+                             POINTER(c_double),  #b
+                             POINTER(c_int),     #jb
+                             POINTER(c_int),     #ib
+                             POINTER(c_double),  #c
+                             POINTER(c_int),     #jc
+                             POINTER(c_int),     #ic
+                             POINTER(c_int),     #nzmax
+                             POINTER(c_int)]     #info 
+_mkl_dcsrmultcsr.restype = None
+
+def mkl_dcsrmultcsr(m, n, k, a, ja, ia, b, jb, ib, trans='N', sort=7):
+
+    trans_point = byref(c_char(bytes(trans)))
+    sort_point = byref(c_int(sort))
+    m_point = byref(c_int(m))
+    n_point = byref(c_int(n))
+    k_point = byref(c_int(k))
+
+    a_point = a.ctypes.data_as(POINTER(c_double))
+    ja_point = ja.ctypes.data_as(POINTER(c_int))
+    ia_point = ia.ctypes.data_as(POINTER(c_int))
+
+    b_point = b.ctypes.data_as(POINTER(c_double))
+    jb_point = jb.ctypes.data_as(POINTER(c_int))
+    ib_point = ib.ctypes.data_as(POINTER(c_int))
+    
+    # First run to determine nnz(C)
+    request = 1
+    request_point = byref(c_int(request))
+
+    c = np.zeros(1, dtype=np.float64)
+    jc = np.zeros(1, dtype=np.int32)
+    ic = np.zeros(m+1, dtype=np.int32)
+    nzmax = 0
+
+    c_point = c.ctypes.data_as(POINTER(c_double))
+    jc_point = jc.ctypes.data_as(POINTER(c_int))
+    ic_point = ic.ctypes.data_as(POINTER(c_int))
+    nzmax_point = byref(c_int(nzmax))
+
+    info  = c_int(-3)
+    info_point  = byref(info)
+
+    _mkl_dcsrmultcsr(trans_point, request_point, sort_point, m_point, 
+                     n_point, k_point, a_point, ja_point, ia_point, 
+                     b_point, jb_point, ib_point, c_point, jc_point, 
+                     ic_point, nzmax_point, info_point) 
+
+    nnz_c = ic[-1]-1
+
+    # Second run to calculate product
+    request = 2
+    request_point = byref(c_int(request))
+
+    c = np.zeros(nnz_c, dtype=np.float64)
+    jc = np.zeros(nnz_c, dtype=np.int32)
+    nzmax = nnz_c
+
+    c_point = c.ctypes.data_as(POINTER(c_double))
+    jc_point = jc.ctypes.data_as(POINTER(c_int))
+    nzmax_point = byref(c_int(nzmax))
+
+    info  = c_int(-3)
+    info_point  = byref(info)
+
+    _mkl_dcsrmultcsr(trans_point, request_point, sort_point, m_point, 
+                     n_point, k_point, a_point, ja_point, ia_point, 
+                     b_point, jb_point, ib_point, c_point, jc_point, 
+                     ic_point, nzmax_point, info_point)
+
+    return c, jc, ic
+
+
+
