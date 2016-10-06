023a70e003ee6059e12de12b1daa041246f34349
Dave Marchant <dave@compgeoinc.com>
2016-10-06 14:30:39 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open('README.rst') as fp:
 
 setup(
     name = 'pyMKL',
-    version = '0.0.1',
+    version = '0.0.2',
     packages = find_packages(),
     install_requires = ['numpy>=1.7',
                         'scipy>=0.13',
