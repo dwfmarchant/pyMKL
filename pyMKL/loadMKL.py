f423be096a9bdb470836bf19c5305c70c4eca8ca
fjbay <69637382+fjbay@users.noreply.github.com>
2021-10-18 14:18:38 +0200

    Fix wrong variable name

--- pyMKL/loadMKL.py
+++ pyMKL/loadMKL.py
@@ -7,12 +7,12 @@ def _loadMKL():
     try:
         # from https://github.com/haasad/PyPardisoProject/commit/98701c21ae5d5b9879c51531ace93c2213c89d55
         if sys.platform == 'darwin':
-            self.libmkl = CDLL('libmkl_rt.dylib')
+            MKLlib = CDLL('libmkl_rt.dylib')
         else:
             # find the correct mkl_rt library by searching the loaded libraries of the process
             proc = psutil.Process(os.getpid())
             mkl_rt = [lib.path for lib in proc.memory_maps() if 'mkl_rt' in lib.path][0]
-            self.libmkl = CDLL(mkl_rt)
+            MKLlib = CDLL(mkl_rt)
     except Exception as e: 
         raise e
 
